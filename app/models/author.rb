3ebbd66233a34637dc63bcff9ef547639ef46562
danielxurbina <danielur1999@gmail.com>
2020-04-06 20:45:17 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true #performs if it's invalid with no name
+    validates :phone_number, length: {minimum: 10} #performs if it's invalid with a short number
+    validates :name, uniqueness: true #performs if it's invalid when non-unique
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true #performs if it's invalid with no title
+    validates :content, length: {minimum: 50} #performs if it's invalid with too short content
+    validates :summary, length: {maximum: 100} #performs if it's invalid with a long summary
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) } #performs if it's invalid with a category outside the choices
+    validate :clickbait?
+
+    @@clickbait_title = [ /Won't Believe/, /Secret/, /Top[0-9]/, /Guess/ ]
+
+    def clickbait?
+        if @@clickbait_title.none? { |click| click.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
